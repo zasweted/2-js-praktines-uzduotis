41588c03e60f00620e42c392a762b4cd6d080c92
Aleksandr Vasiljev <zaswet@gmail.com>
2022-06-08 15:51:31 +0300

    funkcijos

---
+++ js/funkcija.js
@@ -0,0 +1,22 @@
+
+
+function tusciaFunkcija() {
+    return false;
+}
+
+
+console.log(tusciaFunkcija());
+
+
+
+function daugyba(a, b) {
+    return a * b;    
+}
+const a = 11;
+const b = 222;
+const c = 3333;
+
+console.log(daugyba(a, b));
+console.log(daugyba(c, b));
+console.log(daugyba(a, c));
+
