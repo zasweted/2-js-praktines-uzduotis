93615db0d39a2530c82b01b805bd85f6eb5f3bad
Aleksandr Vasiljev <zaswet@gmail.com>
2022-06-09 15:15:15 +0300

    praktines uzdotis

--- js/kintamieji.js
+++ js/kintamieji.js
@@ -165,3 +165,139 @@ if (number2 == number0) {
 } else {
     console.log('Bandikite dar karta');
 }
+
+console.log('---------------------');
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+
+const num = [1 + 2 + 3 + 4 + 5];
+const num1 = [11 + 22 + 33 + 44 + 55];
+const num2 = [111 + 222 + 333 + 444 + 555];
+
+console.log(num);
+console.log(num1);
+console.log(num2);
+
+//Susumuoti visus skaičiaus tipo kintamuosius
+//Rezultatą išvesti į console
+
+
+console.log('start');
+console.log('--------------');
+let suma = 0;
+let suma1 = 0;
+let suma2 = 0;
+
+for (let i=0; i < num.length; i++) {
+    const skaicius = num[i];
+    suma += skaicius;
+    console.log(suma);
+
+}
+console.log('--------------');
+for (let i=0; i < num1.length; i++) {
+    const skaicius1 = num1[i];
+    suma1 += skaicius1;
+    console.log(suma1);
+
+}
+console.log('--------------');
+for (let i=0; i < num2.length; i++) {
+    const skaicius2 = num2[i];
+    suma2 += skaicius2;
+    console.log(suma2);
+
+}
+const sumAll = suma + suma1 + suma2;
+console.log('Final', suma, '+', suma1, '+', suma2, '=', sumAll);
+console.log('--------------');
+console.log('end');
+
+
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const text = ['vienas du trys keturi penki'];
+const text1 = ['vienuolika dvylika trylika keturiolika penkiolika'];
+const text2 = ['1 2 3 4 5'];
+
+console.log(text);
+console.log(text1);
+console.log(text2);
+
+
+//Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+//1-2+3-4+5
+//Rezultatą išvesti į console
+
+const numbers = [1 - 2 + 3 - 4 + 5];
+const numbers1 = [11 - 22 + 33 - 44 + 55];
+const numbers2 = [111 - 222 + 333 - 444 + 555];
+
+console.log(numbers);
+console.log(numbers1);
+console.log(numbers2);
+
+//Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+
+const texts = ['vienas, du, trys, keturi, penki, ',];
+const texts1 = [' vienuolika, dvylika, trylika, keturiolika, penkiolika, ',];
+const texts2 = [' 1, 2, 3, 4, 5'];
+
+const showText = texts + text1 + text2;
+
+console.log(texts);
+console.log(texts1);
+console.log(texts2);
+//Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”.
+
+//Tarpusavyje palyginti skaičiaus tipo kintamuosius:
+//kuris didesnis
+//kuris mažesnis
+//ar jie lygūs
+//ar jie nelygūs
+//kuris didesnis arba lygus
+//kuris mažesnis arba lygus
+
+
+const x = 2;
+const y = 5;
+
+if (x > y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x < y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x === y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x != y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x >= y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
+
+if (x <= y) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar karta');
+}
